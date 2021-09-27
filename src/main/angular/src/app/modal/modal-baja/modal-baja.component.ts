44b62c8947408c309ea70bbf09e6576d27edd3ce
rpovedano <rpovedano@gmail.com>
2021-09-27 23:09:15 +0200

    Arreglado test funcional e importado servicio en las modales

--- src/main/angular/src/app/modal/modal-baja/modal-baja.component.ts
+++ src/main/angular/src/app/modal/modal-baja/modal-baja.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal-baja',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalBajaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
   }
