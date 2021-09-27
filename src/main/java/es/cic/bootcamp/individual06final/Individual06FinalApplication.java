8c86446b16349556a10afb045c65900c432a5a3f
rpovedano <rpovedano@gmail.com>
2021-09-27 09:20:01 +0200

    Configuración inicial de spring, con el mvcconfiguration para el routing del index con angular.

---
+++ src/main/java/es/cic/bootcamp/individual06final/Individual06FinalApplication.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.individual06final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Individual06FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Individual06FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/bootcamp/individual06final/Individual06FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.individual06final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Individual06FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
