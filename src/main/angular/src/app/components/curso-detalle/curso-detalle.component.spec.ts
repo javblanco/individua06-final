73677898a6976d133989374212fef92ecd13a536
rpovedano <rpovedano@gmail.com>
2021-09-28 19:42:07 +0200

    Arreglado test unitarios

--- src/main/angular/src/app/components/curso-detalle/curso-detalle.component.spec.ts
+++ src/main/angular/src/app/components/curso-detalle/curso-detalle.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CursoDetalleComponent } from './curso-detalle.component';
 
@@ -8,7 +11,10 @@ describe('CursoDetalleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CursoDetalleComponent ]
+      declarations: [ CursoDetalleComponent ],
+      imports: [HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule]
     })
     .compileComponents();
   });
--- src/main/angular/src/app/components/curso-maestro/curso-maestro.component.spec.ts
+++ src/main/angular/src/app/components/curso-maestro/curso-maestro.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CursoMaestroComponent } from './curso-maestro.component';
@@ -8,7 +9,8 @@ describe('CursoMaestroComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CursoMaestroComponent ]
+      declarations: [ CursoMaestroComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
